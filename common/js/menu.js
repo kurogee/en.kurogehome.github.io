f9c2c23f1d3924bebfc1597313a071286b79bf11
Kurogee <taiti.nakaya.2009@gmail.com>
2024-07-13 08:46:50 +0900

    一回整理

--- common/js/menu.js
+++
@@ -1,8 +0,0 @@
-const menu_urls = [
-    ["Index", "index.html"],
-    ["About Me", "aboutme.html"],
-    ["Projects", "projects.html"],
-];
-
-const menu = document.getElementById("menu");
-menu.innerHTML = menu_urls.map(([name, url]) => `<a href="./${url}">${name}</a>`).join(" | ");
